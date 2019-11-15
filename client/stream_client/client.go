5bb17939fe01237a7d22535b0a3acf4e2e8e3af4
lxj <14889383@qq.com>
2019-11-15 11:48:05 +0800

    stream

--- client/stream_client/client.go
+++ client/stream_client/client.go
@@ -1,1 +1,101 @@
-package stream_client
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+
+	"google.golang.org/grpc"
+	"rpc_example/proto"
+)
+
+const Addr = ":9003"
+
+func main() {
+	conn, err := grpc.Dial(Addr, grpc.WithInsecure())
+
+	client := proto.NewStreamServiceClient(conn)
+
+	err = printList(client, &proto.StreamRequest{Pt: &proto.StreamPoint{Name: "gPRC stream Client: List", Value: 2018}})
+	if err != nil {
+		log.Fatalf("PrintList.err is %s", err.Error())
+	}
+	err = printRecord(client, &proto.StreamRequest{Pt: &proto.StreamPoint{Name: "gPRC stream Client: Record", Value: 2030}})
+	if err != nil {
+		log.Fatalf("PrintRecord.err is %s", err.Error())
+	}
+	err = printRoute(client, &proto.StreamRequest{Pt: &proto.StreamPoint{Name: "gRPC stream Client: Route", Value: 2020}})
+	if err != nil {
+		log.Fatalf("PrintRoute.err is %s", err.Error())
+	}
+}
+
+func printList(client proto.StreamServiceClient, r *proto.StreamRequest) error {
+	stream, err := client.List(context.Background(), r)
+	if err != nil {
+		return err
+	}
+
+	for true {
+		resp, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		log.Printf("resp: pj.name: %s, pt.value: %d", resp.Pt.Name, resp.Pt.Value)
+	}
+
+	return nil
+}
+
+func printRecord(client proto.StreamServiceClient, r *proto.StreamRequest) error {
+	stream, err := client.Record(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for n := 0; n <= 6; n ++ {
+		err := stream.Send(r)
+		if err != nil {
+			return err
+		}
+	}
+
+	resp, err := stream.CloseAndRecv()
+	if err != nil {
+		return err
+	}
+
+	log.Printf("resp: pj.name: %s, pt.value: %d", resp.Pt.Name, resp.Pt.Value)
+
+	return nil
+}
+
+func printRoute(client proto.StreamServiceClient, r *proto.StreamRequest) error {
+	stream, err := client.Route(context.Background())
+	if err != nil {
+		return err
+	}
+	for n := 0; n <= 6; n ++ {
+		err = stream.Send(r)
+		if err != nil {
+			return err
+		}
+		resp, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		log.Printf("resp: pj.name: %s, pt.value: %d", resp.Pt.Name, resp.Pt.Value)
+	}
+
+	stream.CloseSend()
+
+	return nil
+}
--- server/stream_server/server.go
+++ server/stream_server/server.go
@@ -1,1 +1,80 @@
-package stream_server
+package main
+
+import (
+	"google.golang.org/grpc"
+	"io"
+	"log"
+	"net"
+	"rpc_example/proto"
+)
+
+const ADDRESS = ":9003"
+
+type StreamService struct {}
+
+func (s *StreamService) List(r *proto.StreamRequest, stream proto.StreamService_ListServer) error {
+	for n := 0; n <= 6; n ++ {
+		err := stream.Send(&proto.StreamResponse{
+			Pt: &proto.StreamPoint{Name: r.Pt.Name, Value: r.Pt.Value},
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *StreamService) Record(stream proto.StreamService_RecordServer) error {
+	for true {
+		r, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&proto.StreamResponse{Pt:
+				&proto.StreamPoint{Name: "gRPC Stream Server Record", Value: 1},
+			})
+		}
+		if err != nil {
+			return err
+		}
+		log.Printf("stream.Recv pt.name: %s, pt.value: %d", r.Pt.Name, r.Pt.Value)
+	}
+	
+	return nil
+}
+
+func (s *StreamService) Route(stream proto.StreamService_RouteServer) error {
+	n := 0
+	for true {
+		err := stream.Send(&proto.StreamResponse{Pt: &proto.StreamPoint{Name: "gRPC Stream Server Route", Value: int64(n)}})
+		if err != nil {
+			return err
+		}
+
+		r, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+		n ++
+
+		log.Printf("stream.Revc pt.name: %s, pt.value: %d", r.Pt.Name, r.Pt.Value)
+	}
+
+	return nil
+}
+
+func main() {
+	server := grpc.NewServer()
+
+	proto.RegisterStreamServiceServer(server, &StreamService{})
+
+	lis, err := net.Listen("tcp", ADDRESS)
+	if err != nil {
+		log.Fatalf("net.Listen err %s", err.Error())
+	}
+
+	server.Serve(lis)
+}
